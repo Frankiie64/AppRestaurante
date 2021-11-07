22eac06babb4eb904194d2d95d3a7ac2d00400fe
Franklyn Brea <franklynbrea38@gmail.com>
2021-11-07 15:57:31 -0500

    Add FRM de cantidad personas

---
+++ LogicLayer/Mesa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LogicLayer
+{
+    public class Mesa
+    {
+        object CantidadMesa { get; set; }
+        int CantidadPersona { get; set; }
+        int CantidadOrdenes { get; set; }
+
+        public Mesa(object Mesas, int Personas)
+        {
+            CantidadMesa = Mesas;
+            CantidadPersona = Personas;
+            CantidadOrdenes = Personas;
+
+        }
+
+    }
+}
---
+++ Restaurant_CRUD/FRMLoadMesas.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Restaurant_CRUD
+{
+    partial class FRMLoadMesas
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TLPMain = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // TLPMain
+            // 
+            this.TLPMain.ColumnCount = 3;
+            this.TLPMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLPMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLPMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLPMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TLPMain.Location = new System.Drawing.Point(0, 0);
+            this.TLPMain.Name = "TLPMain";
+            this.TLPMain.RowCount = 3;
+            this.TLPMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLPMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLPMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLPMain.Size = new System.Drawing.Size(800, 450);
+            this.TLPMain.TabIndex = 0;
+            // 
+            // FRMLoadMesas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TLPMain);
+            this.Name = "FRMLoadMesas";
+            this.Text = "Menu Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TLPMain;
+    }
+}
+
